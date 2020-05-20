f52e2d2c3ea8193a9510884ecada130867636cd3
ShweSinHtay <shwesin@isgm2.com>
2020-05-20 13:29:59 +0630

    project added

---
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -0,0 +1,18 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+}
